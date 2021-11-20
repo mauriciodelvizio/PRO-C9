aca6cf540374980d73ce48634994d59ad035616e
mauriciodelvizio <94316853+mauriciodelvizio@users.noreply.github.com>
2021-11-20 11:05:25 -0300

    Add files via upload

---
+++ projeto tela vermelha.js
@@ -0,0 +1,16 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+var sprite = createSprite(200, 200, 10, 10)
+function draw() 
+{
+  background("yellow");
+drawSprites();
+if(keyDown("right")){
+background("red")
+}
+}
+
+
+
